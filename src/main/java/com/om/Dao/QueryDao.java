3b119528fef64b2c4653608bf64d55c1128a84f5
lixianlin01 <xiaoliwow@163.com>
2022-03-22 14:26:01 +0800

    add maintainer`s gitee_id and email for sig details

--- src/main/java/com/om/Dao/QueryDao.java
+++ src/main/java/com/om/Dao/QueryDao.java
@@ -1354,7 +1354,7 @@ public class QueryDao {
                 String company = bucket.get("key").asText();
                 long contribute = bucket.get("sum_field").get("value").asLong();
                 if (/*!claCompanys.contains(company) || */contribute == 0) {
-                    independent += contribute;
+//                    independent += contribute;
                     continue;
                 }
                 String companyCn = companyNameAlCn.getOrDefault(company.trim(), company.trim());
@@ -1365,9 +1365,9 @@ public class QueryDao {
                 JsonNode resNode = objectMapper.valueToTree(dataMap);
                 dataList.add(resNode);
             }
-            dataMap.put("company_cn", "independent");
-            dataMap.put("company_en", "independent");
-            dataMap.put("contribute", independent);
+//            dataMap.put("company_cn", "independent");
+//            dataMap.put("company_en", "independent");
+//            dataMap.put("contribute", independent);
             JsonNode resNode = objectMapper.valueToTree(dataMap);
             dataList.add(resNode);
 
