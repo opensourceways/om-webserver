a928c4e2e42c2163ba32b476385f8ae813a0b5f9
Administrator <sch_na@163.com>
2022-05-31 11:56:59 +0800

    Add APIs/add get company users counts, add sig feature

---
+++ src/main/java/com/om/Modules/yaml/SigYamlInfo.java
@@ -0,0 +1,24 @@
+package com.om.Modules.yaml;
+
+import java.util.List;
+
+public class SigYamlInfo {
+    private String name;
+    private List<String> sigs;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getSigs() {
+        return sigs;
+    }
+
+    public void setSigs(List<String> sigs) {
+        this.sigs = sigs;
+    }
+}
