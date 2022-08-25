3c37273ea51ad4b93d95c19947ff3909878d0194
lixianlin01 <xiaoliwow@163.com>
2022-08-25 17:17:53 +0800

    get company name yaml to local

--- src/main/java/com/om/Utils/YamlUtil.java
+++ src/main/java/com/om/Utils/YamlUtil.java
@@ -2,7 +2,7 @@ package com.om.Utils;
 
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 
 public class YamlUtil {
@@ -18,5 +18,86 @@ public class YamlUtil {
             e.printStackTrace();
         }
         return t;
+    }
+
+    public <T> T readLocalYaml(String yamlFile, Class<T> classType) {
+        Yaml yaml = new Yaml();
+        InputStream inputStream;
+        T t = null;
+        try {
+            inputStream = new FileInputStream(yamlFile);
+            t = yaml.loadAs(inputStream, classType);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
+    public String wget(String urlFile, String path) {
+        Runtime runtime = Runtime.getRuntime();
+        String cmd = String.format("wget -N -P %s %s", path, urlFile);
+        System.out.println("cmd: " + cmd);
+
+        String[] sp = urlFile.split("/");
+        String localFile = path + sp[sp.length - 1];
+
+        try {
+            Process process = runtime.exec(cmd);
+            InputStream is1 = process.getInputStream();
+            InputStream is2 = process.getErrorStream();
+            new Thread() {
+                @Override
+                public void run() {
+                    BufferedReader bi = null;
+                    String line;
+                    try {
+                        bi = new BufferedReader(new InputStreamReader(is1));
+                        ;
+                        while ((line = bi.readLine()) != null) {
+                            System.out.println(line);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (bi != null) {
+                            try {
+                                bi.close();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }.start();
+            new Thread() {
+                @Override
+                public void run() {
+                    BufferedReader bi = null;
+                    String line;
+                    try {
+                        bi = new BufferedReader(new InputStreamReader(is2));
+                        ;
+                        while ((line = bi.readLine()) != null) {
+                            System.out.println(line);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (bi != null) {
+                            try {
+                                bi.close();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }.start();
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return localFile;
     }
 }
