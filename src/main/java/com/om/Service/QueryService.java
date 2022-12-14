1e6d8d7671ccbbd7d5a3dd6cf23059096af639a2
Administrator <sch_na@163.com>
2022-12-14 16:09:56 +0800

    remove dead code

--- src/main/java/com/om/Service/QueryService.java
+++ src/main/java/com/om/Service/QueryService.java
@@ -1416,23 +1416,5 @@ public class QueryService {
         }
         return result;
     }
-    public String queryMindsporeSig(String lang) {
-        String key = "MindsporeSig" + lang.toLowerCase();
-        String result = null;
-        result =  (String) redisDao.get(key);
-        if (result == null) {
-            // 查询数据库，更新redis 缓存。
-            try {
-                result = queryDao.getMindsporeSigList(lang);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            boolean set =  redisDao.set(key, result, Long.valueOf(Objects.requireNonNull(env.getProperty("spring.redis.key.expire"))));
-            if (set) {
-                System.out.println("update " + key + " success!");
-            }
-        }
-        return result;
-    }
 }
 
