99ba2d5acf140bde4abdb3a7889e49df9c31dd80
陈阳 <775517776@qq.com>
2023-06-13 17:30:04 +0800

    加入审计日志

---
+++ src/main/java/com/om/Vo/ManagementLog.java
@@ -0,0 +1,34 @@
+package com.om.Vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class ManagementLog implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String type;
+
+    private String time;
+
+    private String func;
+
+    private String eventDetails;
+
+    private String requestUrl;
+
+    private String method;
+
+    private String appIP;
+
+    private int status;
+
+    private String message;
+
+    private String ErrorLog;
+
+}
