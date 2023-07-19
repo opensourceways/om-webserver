b691254b4e5a5d9169031cf9eb1ed2b61d7ba587
wangweifeng <wangweifeng21@huawei.com>
2023-07-19 10:36:15 +0800

    Add consent accept term check

--- src/main/java/com/om/Result/Constant.java
+++ src/main/java/com/om/Result/Constant.java
@@ -75,5 +75,6 @@ public class Constant {
     public static final String ONEID_TOKEN_PATH = "/auth/get-management-token";
 
     public static final String PHOTO_NOT_ALLOWED_CHARS = "\\,/,:,*,?,\",<,>,|";
+    public static final String CONSENT_ACCEPT_TERM = "0";
 }
 
