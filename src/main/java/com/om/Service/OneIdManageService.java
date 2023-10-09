c1521bf8e761b450063b570b823eab58269179b7
wangweifeng <wangweifeng21@huawei.com>
2023-10-09 17:31:09 +0800

    change log level

--- src/main/java/com/om/Service/OneIdManageService.java
+++ src/main/java/com/om/Service/OneIdManageService.java
@@ -105,7 +105,7 @@ public class OneIdManageService {
         String channel = body.get("channel");
 
         // debug
-        logger.debug(body.get("captchaVerification"), body.get("account"), body.get("channel"));
+        logger.error(body.get("captchaVerification"), body.get("account"), body.get("channel"));
 
         // 图片验证码二次校验
         if (!isSuccess) {
