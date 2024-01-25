7aa451891eab0da08b8d382818ffe3da8456d74b
陈阳 <775517776@qq.com>
2024-01-25 15:07:35 +0800

    三方用登录 注册 接口

---
+++ src/main/java/com/om/Dao/oneId/OneIdThirdPartyUserDao.java
@@ -0,0 +1,44 @@
+package com.om.Dao.oneId;
+
+import com.alibaba.fastjson2.JSON;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.om.Result.Constant;
+import org.json.JSONObject;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OneIdThirdPartyUserDao {
+
+    public OneIdEntity.ThirdPartyUser getThirdPartyUserByProvider(String provider, String userIdInIdp) throws Exception {
+        String url = OneIdConfig.API_HOST + Constant.ONEID_THIRD_PARTY_USER_GET_PROVIDER_PATH;
+        HttpResponse<JsonNode> response = Unirest.get(url)
+                .header("Authorization",  OneIdConfig.getManagementToken())
+                .queryString("provider", provider)
+                .queryString("userIdInIdp", userIdInIdp)
+                .asJson();
+
+        if (response.getStatus() == 200) {
+            JSONObject jsonObject = response.getBody().getObject().getJSONObject("data");
+            return JSON.parseObject(jsonObject.toString(), OneIdEntity.ThirdPartyUser.class);
+        }
+        return null;
+    }
+
+
+    public OneIdEntity.User createCompositeUser(OneIdEntity.User user) throws Exception {
+        String url = OneIdConfig.API_HOST + Constant.ONEID_USER_C_PATH;
+        HttpResponse<JsonNode> response = Unirest.post(url)
+                .header("Content-Type", "application/json")
+                .header("Authorization",  OneIdConfig.getManagementToken())
+                .body(JSON.toJSONString(user))
+                .asJson();
+        if (response.getStatus() == 200) {
+            JSONObject jsonObject = response.getBody().getObject().getJSONObject("data");
+            return JSON.parseObject(jsonObject.toString(), OneIdEntity.User.class);
+        }
+        return null;
+    }
+
+}
--- src/main/java/com/om/Modules/MessageCodeConfig.java
+++ src/main/java/com/om/Modules/MessageCodeConfig.java
@@ -85,6 +85,8 @@ public enum MessageCodeConfig {
     E00064("E00064", "Invalid redirect url", "无效的回调地址"),
     E00065("E00065", "Unsupported scope", "不支持的范围"),
     E00066("E00066", "Fail to get social identities corresponding to app id", "获取应用外部身份源失败"),
+    E00067("E00067", "The third-party user information already exists and cannot be created", "已有该三方用户信息，无法新建"),
+    E00068("E00068", "The third-party user creation failed", "三方用户信息创建未成功 "),
 
     OIDC_E00001("OIDC_E00001", "currently response_type only supports code", "目前 response_type 仅支持 code"),
     OIDC_E00002("OIDC_E00002", "redirect_uri not found in the app", "该a应用未配置这个回调路由"),
