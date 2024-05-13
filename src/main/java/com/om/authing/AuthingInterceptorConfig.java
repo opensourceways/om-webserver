e70a7ec310c7f14be65d22bfcc2294947d1533be
fuxinji9527 <1992666531@qq.com>
2024-05-13 11:29:33 +0800

    代码规范整改

--- src/main/java/com/om/authing/AuthingInterceptorConfig.java
+++ src/main/java/com/om/authing/AuthingInterceptorConfig.java
@@ -16,16 +16,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * WebMvcConfigurer 接口实现类，用于配置和注册 Authing 拦截器.
+ */
 @Configuration
 public class AuthingInterceptorConfig implements WebMvcConfigurer {
+
+    /**
+     * 添加拦截器到拦截器注册表中.
+     *
+     * @param registry 拦截器注册表
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(AuthingInterceptor())
+        registry.addInterceptor(authingInterceptor())
                 .addPathPatterns("/query/**", "/oneid/**");
     }
 
+    /**
+     * 创建 AuthingInterceptor Bean 实例.
+     *
+     * @return 返回创建的 AuthingInterceptor Bean
+     */
     @Bean
-    public AuthingInterceptor AuthingInterceptor() {
+    public AuthingInterceptor authingInterceptor() {
         return new AuthingInterceptor();
     }
 }
