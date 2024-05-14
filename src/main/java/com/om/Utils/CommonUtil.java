2ba24cdfed9fa552f5b3e84e366eca52b5a43d93
fuxinji9527 <1992666531@qq.com>
2024-05-14 15:57:08 +0800

    代码规范整改

--- src/main/java/com/om/Utils/CommonUtil.java
+++ src/main/java/com/om/Utils/CommonUtil.java
@@ -12,10 +12,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
-public class CommonUtil {
+public final class CommonUtil {
 
-    private static final Logger logger =  LoggerFactory.getLogger(CommonUtil.class);
-    
+
+    private CommonUtil() {
+        throw new AssertionError("Utility class. Not intended for instantiation.");
+    }
+
+    /**
+     * 日志记录器，用于记录 CommonUtil 类的日志信息.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommonUtil.class);
+
+    /**
+     * 检查文件内容类型是否有效.
+     *
+     * @param file 要检查的文件
+     * @return 如果文件内容类型有效，则返回 true；否则返回 false
+     * @throws IOException 当发生输入输出异常时抛出
+     */
     public static boolean isFileContentTypeValid(MultipartFile file) throws IOException {
         try {
             byte[] fileContent = file.getBytes();
@@ -23,18 +38,26 @@ public class CommonUtil {
             // 验证文件头信息
             if (fileContent.length >= 2 && fileContent[0] == (byte) 0xFF && fileContent[1] == (byte) 0xD8) {
                 return file.getOriginalFilename().toString().toLowerCase().endsWith(".jpg");
-            } else if (fileContent.length >= 8 && fileContent[0] == (byte) 0x89 &&
-                    fileContent[1] == 'P' && fileContent[2] == 'N' && fileContent[3] == 'G' &&
-                    fileContent[4] == 0x0D && fileContent[5] == 0x0A && fileContent[6] == 0x1A && fileContent[7] == 0x0A) {
+            } else if (fileContent.length >= 8 && fileContent[0] == (byte) 0x89
+                    && fileContent[1] == 'P' && fileContent[2] == 'N' && fileContent[3] == 'G'
+                    && fileContent[4] == 0x0D && fileContent[5] == 0x0A && fileContent[6] == 0x1A
+                    && fileContent[7] == 0x0A) {
                 return file.getOriginalFilename().toString().toLowerCase().endsWith(".png");
             }
         } catch (Exception e) {
-            logger.error(e.getMessage());
+            LOGGER.error(e.getMessage());
         }
         return false;
     }
-    
-    public static InputStream rewriteImage(MultipartFile file) throws IOException{
+
+    /**
+     * 重写图像文件并返回输入流.
+     *
+     * @param file 要重写的图像文件
+     * @return 重写后的图像文件的输入流
+     * @throws IOException 当发生输入输出异常时抛出
+     */
+    public static InputStream rewriteImage(MultipartFile file) throws IOException {
         try {
             byte[] fileContent = check(file.getBytes());
             ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(fileContent);
@@ -46,6 +69,12 @@ public class CommonUtil {
         }
     }
 
+    /**
+     * 删除文件.
+     *
+     * @param path 文件路径
+     * @return 如果文件删除成功则返回 true，否则返回 false
+     */
     public static boolean deleteFile(String path) {
         File file = new File(path);
         if (file.exists()) {
--- src/main/java/com/om/Utils/RSAUtil.java
+++ src/main/java/com/om/Utils/RSAUtil.java
@@ -14,7 +14,12 @@ package com.om.Utils;
 import java.io.ByteArrayOutputStream;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
-import java.security.*;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
@@ -24,6 +29,7 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,24 +38,47 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RSAUtil implements Serializable {
-    public static String KEY_ALGORITHM;
-    
-    public static String RSA_ALGORITHM;
-
-    @Value("${rsa.key.algorithm:RSA}")
-    public void setKeyAlgorithm(String keyAlgorithm) { RSAUtil.KEY_ALGORITHM = keyAlgorithm; }
-
-    @Value("${rsa.authing.algorithm:RSA/ECB/OAEPWithSHA-256AndMGF1Padding}")
-    public void setRsaAlgorithm(String rsaAlgorithm) { RSAUtil.RSA_ALGORITHM = rsaAlgorithm; }
+    /**
+     * 密钥算法.
+     */
+    private static String keyAlgorithm;
 
     /**
-     * 随机生成密钥对(公钥和私钥)
+     * RSA算法.
+     */
+    private static String rsaAlgorithm;
+
+
+    /**
+     * 设置密钥算法.
      *
-     * @param keySize 密钥长度
+     * @param keyAlgorithm 密钥算法
+     */
+    @Value("${rsa.key.algorithm:RSA}")
+    public void setKeyAlgorithm(String keyAlgorithm) {
+        RSAUtil.keyAlgorithm = keyAlgorithm;
+    }
+
+    /**
+     * 设置 RSA 算法.
+     *
+     * @param rsaAlgorithm RSA 算法
+     */
+    @Value("${rsa.authing.algorithm:RSA/ECB/OAEPWithSHA-256AndMGF1Padding}")
+    public void setRsaAlgorithm(String rsaAlgorithm) {
+        RSAUtil.rsaAlgorithm = rsaAlgorithm;
+    }
+
+    /**
+     * 随机生成密钥对(公钥和私钥).
+     *
+     * @param keySize 密钥大小
+     * @return 包含公钥和私钥的映射
+     * @throws NoSuchAlgorithmException 当算法不存在时抛出异常
      */
     public static Map<String, String> createKeys(int keySize) throws NoSuchAlgorithmException {
         //为RSA算法创建一个KeyPairGenerator对象
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance(RSA_ALGORITHM);
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(rsaAlgorithm);
         //初始化KeyPairGenerator对象,密钥长度
         kpg.initialize(keySize);
         //生成密匙对
@@ -68,84 +97,118 @@ public class RSAUtil implements Serializable {
     }
 
     /**
-     * 获取公钥
+     * 获取公钥.
      *
-     * @param publicKey 密钥字符串（经过base64编码）
+     * @param publicKey 公钥字符串
+     * @return RSAPublicKey 对象
+     * @throws NoSuchAlgorithmException 当算法不存在时抛出异常
+     * @throws InvalidKeySpecException  当密钥规范无效时抛出异常
      */
     public static RSAPublicKey getPublicKey(String publicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
         // 通过X509编码的Key指令获得公钥对象
-        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        KeyFactory keyFactory = KeyFactory.getInstance(keyAlgorithm);
         X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(Base64.decodeBase64(publicKey));
         return (RSAPublicKey) keyFactory.generatePublic(x509KeySpec);
     }
 
     /**
-     * 获取私钥
+     * 获取私钥.
      *
-     * @param privateKey 密钥字符串（经过base64编码）
+     * @param privateKey 私钥字符串
+     * @return RSAPrivateKey 对象
+     * @throws NoSuchAlgorithmException 当算法不存在时抛出异常
+     * @throws InvalidKeySpecException  当密钥规范无效时抛出异常
      */
-    public static RSAPrivateKey getPrivateKey(String privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public static RSAPrivateKey getPrivateKey(String privateKey) throws NoSuchAlgorithmException,
+            InvalidKeySpecException {
         //通过PKCS#8编码的Key指令获得私钥对象
-        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        KeyFactory keyFactory = KeyFactory.getInstance(keyAlgorithm);
         PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(privateKey));
         return (RSAPrivateKey) keyFactory.generatePrivate(pkcs8KeySpec);
     }
 
     /**
-     * 公钥加密
+     * 公钥加密.
      *
      * @param data      明文
      * @param publicKey 公钥
+     * @return 加密后的数据
+     * @throws NoSuchPaddingException   当填充方式不存在时抛出异常
+     * @throws NoSuchAlgorithmException 当算法不存在时抛出异常
+     * @throws InvalidKeyException      当密钥无效时抛出异常
      */
-    public static String publicEncrypt(String data, RSAPublicKey publicKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+    public static String publicEncrypt(String data, RSAPublicKey publicKey) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidKeyException {
+
+        Cipher cipher = Cipher.getInstance(rsaAlgorithm);
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        return Base64.encodeBase64URLSafeString(rsaSplitCodec(cipher, Cipher.ENCRYPT_MODE, data.getBytes(StandardCharsets.UTF_8), publicKey.getModulus().bitLength()));
+        return Base64.encodeBase64URLSafeString(rsaSplitCodec(cipher, Cipher.ENCRYPT_MODE,
+                data.getBytes(StandardCharsets.UTF_8), publicKey.getModulus().bitLength()));
     }
 
     /**
-     * 私钥解密
+     * 私钥解密.
      *
      * @param data       密文
      * @param privateKey 私钥
+     * @return 解密后的数据
+     * @throws NoSuchPaddingException   当填充方式不存在时抛出异常
+     * @throws NoSuchAlgorithmException 当算法不存在时抛出异常
+     * @throws InvalidKeyException      当密钥无效时抛出异常
      */
-    public static String privateDecrypt(String data, RSAPrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+    public static String privateDecrypt(String data, RSAPrivateKey privateKey) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidKeyException {
+        Cipher cipher = Cipher.getInstance(rsaAlgorithm);
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        return new String(rsaSplitCodec(cipher, Cipher.DECRYPT_MODE, Base64.decodeBase64(data), privateKey.getModulus().bitLength()), StandardCharsets.UTF_8);
+        return new String(rsaSplitCodec(cipher, Cipher.DECRYPT_MODE,
+                Base64.decodeBase64(data), privateKey.getModulus().bitLength()), StandardCharsets.UTF_8);
     }
 
+
     /**
-     * 私钥加密
+     * 私钥加密.
      *
      * @param data       明文
      * @param privateKey 私钥
+     * @return 加密后的数据
+     * @throws NoSuchPaddingException   当填充方式不存在时抛出异常
+     * @throws NoSuchAlgorithmException 当算法不存在时抛出异常
+     * @throws InvalidKeyException      当密钥无效时抛出异常
      */
-    public static String privateEncrypt(String data, RSAPrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+    public static String privateEncrypt(String data, RSAPrivateKey privateKey) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidKeyException {
+        Cipher cipher = Cipher.getInstance(rsaAlgorithm);
         cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-        return Base64.encodeBase64URLSafeString(rsaSplitCodec(cipher, Cipher.ENCRYPT_MODE, data.getBytes(StandardCharsets.UTF_8), privateKey.getModulus().bitLength()));
+        return Base64.encodeBase64URLSafeString(rsaSplitCodec(cipher, Cipher.ENCRYPT_MODE,
+                data.getBytes(StandardCharsets.UTF_8), privateKey.getModulus().bitLength()));
     }
 
     /**
-     * 公钥解密
+     * 公钥解密.
      *
      * @param data      密文
      * @param publicKey 公钥
+     * @return 解密后的数据
+     * @throws NoSuchPaddingException   当填充方式不存在时抛出异常
+     * @throws NoSuchAlgorithmException 当算法不存在时抛出异常
+     * @throws InvalidKeyException      当密钥无效时抛出异常
      */
-    public static String publicDecrypt(String data, RSAPublicKey publicKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+    public static String publicDecrypt(String data, RSAPublicKey publicKey) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidKeyException {
+        Cipher cipher = Cipher.getInstance(rsaAlgorithm);
         cipher.init(Cipher.DECRYPT_MODE, publicKey);
-        return new String(rsaSplitCodec(cipher, Cipher.DECRYPT_MODE, Base64.decodeBase64(data), publicKey.getModulus().bitLength()), StandardCharsets.UTF_8);
+        return new String(rsaSplitCodec(cipher, Cipher.DECRYPT_MODE,
+                Base64.decodeBase64(data), publicKey.getModulus().bitLength()), StandardCharsets.UTF_8);
     }
 
     /**
-     * 对数据分段加密码、解密
+     * 对数据分段加密码、解密.
      *
      * @param cipher  密码服务
      * @param opmode  加密 or 解密
      * @param datas   需要加密或者解密的内容
      * @param keySize 密钥长度
+     * @return 编解码后的数据数组
      */
     private static byte[] rsaSplitCodec(Cipher cipher, int opmode, byte[] datas, int keySize) {
         int maxBlock = 0;
@@ -178,13 +241,5 @@ public class RSAUtil implements Serializable {
         byte[] dataResult = out.toByteArray();
         IOUtils.closeQuietly(out);
         return dataResult;
-    }
-
-    public static void main(String[] args) throws Exception {
-        Map<String, String> keyMap = RSAUtil.createKeys(3072);
-        String publicKey = keyMap.get("publicKey");
-        String privateKey = keyMap.get("privateKey");
-        System.out.println("公钥: \n\r" + publicKey);
-        System.out.println("私钥： \n\r" + privateKey);
     }
 }
