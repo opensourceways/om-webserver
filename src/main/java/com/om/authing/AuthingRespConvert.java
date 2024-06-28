73a2af4d3d1eeac410cb04be2aa1e771de54b1aa
tfhddd <2272751277@qq.com>
2024-06-28 17:33:46 +0800

    增加可以对外展示的authing返回信息

--- src/main/java/com/om/authing/AuthingRespConvert.java
+++ src/main/java/com/om/authing/AuthingRespConvert.java
@@ -40,6 +40,7 @@ public final class AuthingRespConvert {
     private static final Map<Integer, String> API_CODE_MAP = Collections.unmodifiableMap(
             new HashMap<>() {
                 {
+                    put(500, "验证码不正确");
                     put(1576, "无权限登录此应用，请联系管理员");
                     put(1639, "首次登录重置密码（不需要验证旧密码）");
                     put(1640, "触发联邦认证询问身份绑定，只允许绑定现有账号");
@@ -56,12 +57,15 @@ public final class AuthingRespConvert {
                     put(1706, "人脸已被绑定");
                     put(1707, "绑定人脸失败");
                     put(2000, "须要图形验证码或图形验证码错误");
+                    put(2001, "验证码已失效，请重新获取验证码");
                     put(2004, "用户不存在");
                     put(2005, "用户已锁定");
                     put(2006, "密码错误");
                     put(2020, "尚未登录，无权限访问此请求");
+                    put(2026, "用户已存在，请直接登录");
                     put(2031, "应用已禁止注册用户");
                     put(2058, "强制重置密码（需要验证旧密码）");
+                    put(2059, "密码重复设置");
                     put(2333, "用户名或密码错误");
                     put(2334, "AD 用户验证失败");
                     put(2338, "AD 账号或密码错误");
