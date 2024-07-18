6f7643af635b4d070632821ee7bd32f94b481306
xingzhizhaowu <1519855433@qq.com>
2024-07-18 17:32:25 +0800

    [fix] 解决登陆失败时无法清除子域名的cookie，引发下次登陆也失败问题

--- src/main/java/com/om/authing/AuthingInterceptor.java
+++ src/main/java/com/om/authing/AuthingInterceptor.java
@@ -56,6 +56,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 拦截器类，用于进行身份验证拦截.
@@ -482,6 +484,13 @@ public class AuthingInterceptor implements HandlerInterceptor {
     private void tokenError(HttpServletRequest httpServletRequest,
                             HttpServletResponse httpServletResponse,
                             String message) throws IOException {
+        String refrer = httpServletRequest.getHeader("referer");
+        List<String> childDomains = HttpClientUtils.extractSubdomains(refrer);
+        domain2secure.putAll(childDomains.stream().collect(Collectors.toMap(
+                            Function.identity(), // keyMapper，直接返回元素本身作为key
+                            item -> true, // valueMapper，每个key对应的value都是true
+                            (existing, replacement) -> existing
+                    )));
         HttpClientUtils.setCookie(httpServletRequest, httpServletResponse, cookieTokenName,
                 null, true, 0, "/", domain2secure);
 
