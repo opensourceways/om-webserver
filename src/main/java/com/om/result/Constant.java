34f9f2b2707d29f40aafc8166ab72406e95504ca
tfhddd <2272751277@qq.com>
2024-09-03 10:43:18 +0800

    [fix]将不安全的rsa密码加密算法更换为国密

--- src/main/java/com/om/result/Constant.java
+++ src/main/java/com/om/result/Constant.java
@@ -85,6 +85,11 @@ public final class Constant {
     public static final String OPEN_MIND_USERNAME_REGEX = "^[a-zA-Z]([-_.]([a-zA-Z0-9])|[a-zA-Z0-9])+$";
 
     /**
+     * 字母或数字.
+     */
+    public static final String NORMAL_STR_REGEX = "[a-zA-Z0-9]+";
+
+    /**
      * openmind用户名最小长度.
      */
     public static final int OPEN_MIND_USERNAME_MIN = 3;
