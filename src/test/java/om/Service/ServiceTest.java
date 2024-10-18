6ebc5eb3126b13714940a08700bb79bdbe82d40b
G-Sacker <ljx11714472@gmail.com>
2024-10-18 16:04:29 +0800

    【oneid】【冗余代码】删除冗余代码代码

--- src/test/java/om/Service/ServiceTest.java
+++ src/test/java/om/Service/ServiceTest.java
@@ -30,19 +30,6 @@ public class ServiceTest {
     @Test
     public void authingUserPermission() {
         try {
-//            ResponseEntity res1 = AuthingService.authingUserPermission("openeuler", "884", "sigRead", null);
-//            ResponseEntity res2 = AuthingService.authingUserPermission("openeuler", "84", "sigRead", null);
-//            ResponseEntity res3 = AuthingService.authingUserPermission("openeuler", "83", "sigRead", null);
-//
-//            Assert.assertEquals(res1.getStatusCodeValue(), 401);
-//            Assert.assertEquals(res2.getStatusCodeValue(), 401);
-//            Assert.assertEquals(res3.getStatusCodeValue(), 401);
-
-//            JsonNode jsonNode1 = objectMapper.readTree(objectMapper.writeValueAsString(res1.getBody()));
-//            JsonNode jsonNode3 = objectMapper.readTree(objectMapper.writeValueAsString(res3.getBody()));
-//
-//            Assert.assertEquals(jsonNode1.get("data").get("permissions").size(), 0);
-//            Assert.assertEquals(jsonNode3.get("data").get("permissions").size(), 1);
 
         } catch (Exception e) {
             e.printStackTrace();
