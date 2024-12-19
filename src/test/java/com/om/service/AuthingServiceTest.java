70031045050012527176b7fcae9fb448341ae662
gi1tee <gouzhonglin@huawei.com>
2024-12-19 11:20:04 +0800

    pr33

--- src/test/java/com/om/service/AuthingServiceTest.java
+++ src/test/java/com/om/service/AuthingServiceTest.java
@@ -11,17 +11,81 @@
 
 package com.om.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.om.dao.AuthingManagerDao;
+import com.om.dao.AuthingUserDao;
+import com.om.dao.RedisDao;
+import com.om.modules.ServerErrorException;
+import com.om.modules.authing.AuthingAppSync;
+import com.om.token.ClientSessionManager;
+import com.om.utils.AuthingUtil;
+import com.om.utils.LimitUtil;
+
+import cn.authing.core.types.Application;
+import cn.authing.core.types.ApplicationPermissionStrategyConfig;
+import cn.authing.core.types.ApplicationQRCodeScanning;
+import cn.authing.core.types.ISsoPageCustomizationSettings;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class AuthingServiceTest {
+    @InjectMocks
+    private AuthingService authingService;
+
+    @Mock
+    private AuthingUtil authingUtil;
+
+    @Mock
+    private Environment env;
+
+    @Mock
+    private AuthingUserDao authingUserDao;
+
+    @Mock
+    private RedisDao redisDao;
+
+    @Mock
+    private JavaMailSender mailSender;
+
+    @Mock
+    private LimitUtil limitUtil;
+
+    @Mock
+    private JwtTokenCreateService jwtTokenCreateService;
+
+    @Mock
+    private AuthingAppSync authingAppSync;
+
+    @Mock
+    private ClientSessionManager clientSessionManager;
+
+    @Mock
+    private AuthingManagerDao authingManagerDao;
+
     @Test
     public void testGetAbsoluteAccountNormal() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         AuthingService instance = new AuthingService();
@@ -65,5 +129,107 @@ public class AuthingServiceTest {
 
         result = (String) privateMethod.invoke(instance, "test@qq.com");
         assertEquals("test@qq.com", result);
+    }
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        ReflectionTestUtils.setField(authingService, "authingUserDao", authingUserDao);
+        ReflectionTestUtils.setField(authingService, "env", env);
+        authingService.init();
+    }
+    @Test
+    public void testAccountExists_AppDoesNotExist() throws ServerErrorException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("username")).thenReturn("testuser");
+        when(request.getParameter("client_id")).thenReturn("nonexistent_app");
+        Application app = null; // Simulate non-existent app
+        when(authingUserDao.getAppById(anyString())).thenReturn(app);
+        ResponseEntity<?> result = authingService.accountExists(request, response);
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+    @Test
+    public void testAccountExists_UsernameBlank() throws ServerErrorException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("username")).thenReturn("");
+        when(request.getParameter("client_id")).thenReturn("existing_app");
+        List<String> redirectUri = new ArrayList<>();
+        List<String> logoutUri = new ArrayList<>();
+        ISsoPageCustomizationSettings iSsoPageCustomizationSettings = new ISsoPageCustomizationSettings(false,
+                false, false, false, false, false, false, false,
+                false, false);
+        ApplicationQRCodeScanning applicationQRCodeScanning = new ApplicationQRCodeScanning(false, 1);
+        ApplicationPermissionStrategyConfig permissionStrategyConfig = new ApplicationPermissionStrategyConfig(false,
+                "", "", "");
+        Application app = new Application("4234", "324", "", false, false, false, "", "", "fdsf",
+                "", "", iSsoPageCustomizationSettings, "", redirectUri, logoutUri, false, false, false,
+                false, new ArrayList<>(), "", new ArrayList<>(), "",
+                new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                false, new ArrayList<>(), new ArrayList<>(), "",
+                applicationQRCodeScanning, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                false, permissionStrategyConfig, false);
+        when(authingUserDao.getAppById(anyString())).thenReturn(app);
+        ResponseEntity<?> result = authingService.accountExists(request, response);
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+    @Test
+    public void testAccountExists_UserAlreadyExists() throws ServerErrorException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("username")).thenReturn("existing_user");
+        when(request.getParameter("client_id")).thenReturn("existing_app");
+        List<String> redirectUri = new ArrayList<>();
+        List<String> logoutUri = new ArrayList<>();
+        ISsoPageCustomizationSettings iSsoPageCustomizationSettings = new ISsoPageCustomizationSettings(false,
+                false, false, false, false, false, false, false,
+                false, false);
+        ApplicationQRCodeScanning applicationQRCodeScanning = new ApplicationQRCodeScanning(false, 1);
+        ApplicationPermissionStrategyConfig permissionStrategyConfig = new ApplicationPermissionStrategyConfig(false,
+                "", "", "");
+        Application app = new Application("4234", "324", "", false, false, false, "", "", "fdsf",
+                "", "", iSsoPageCustomizationSettings, "", redirectUri, logoutUri, false, false, false,
+                false, new ArrayList<>(), "", new ArrayList<>(), "",
+                new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                false, new ArrayList<>(), new ArrayList<>(), "",
+                applicationQRCodeScanning, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                false, permissionStrategyConfig, false);
+        when(authingUserDao.getAppById(anyString())).thenReturn(app);
+        boolean userExists = true; // Simulate user already exists
+        when(authingUserDao.isUserExists(anyString(), anyString(), anyString())).thenReturn(userExists);
+        ResponseEntity<?> result = authingService.accountExists(request, response);
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+    @Test
+    public void testAccountExists_Success() throws ServerErrorException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("username")).thenReturn("new_user");
+        when(request.getParameter("client_id")).thenReturn("existing_app");
+        List<String> redirectUri = new ArrayList<>();
+        List<String> logoutUri = new ArrayList<>();
+        ISsoPageCustomizationSettings iSsoPageCustomizationSettings = new ISsoPageCustomizationSettings(false,
+                false, false, false, false, false, false, false,
+                false, false);
+        ApplicationQRCodeScanning applicationQRCodeScanning = new ApplicationQRCodeScanning(false, 1);
+        ApplicationPermissionStrategyConfig permissionStrategyConfig = new ApplicationPermissionStrategyConfig(false,
+                "", "", "");
+        Application app = new Application("4234", "324", "", false, false, false, "", "", "fdsf",
+                "", "", iSsoPageCustomizationSettings, "", redirectUri, logoutUri, false, false, false,
+                false, new ArrayList<>(), "", new ArrayList<>(), "",
+                new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                false, new ArrayList<>(), new ArrayList<>(), "",
+                applicationQRCodeScanning, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                false, permissionStrategyConfig, false);
+        when(authingUserDao.getAppById(anyString())).thenReturn(app);
+        boolean userExists = false; // Simulate user does not exist
+        when(authingUserDao.isUserExists(anyString(), anyString(), anyString())).thenReturn(userExists);
+        ResponseEntity<?> result = authingService.accountExists(request, response);
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("success", ((Map<?, ?>) result.getBody()).get("msg"));
     }
 }
